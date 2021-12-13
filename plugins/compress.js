15d554742672862f13f1f1e13388ad8ed49d00f8
torstah <tor@plan8.se>
2021-12-13 16:35:36 +0100

    zip handle filenames

--- plugins/compress.js
+++ plugins/compress.js
@@ -1,5 +1,23 @@
-import JSZip from "jszip";
+import JSZip, { file } from "jszip";
 import saveAs from "file-saver";
+
+function string_to_slug (str) {
+  str = str.replace(/^\s+|\s+$/g, ''); // trim
+  str = str.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+  var to   = "aaaaeeeeiiiioooouuuunc------";
+  for (var i=0, l=from.length ; i<l ; i++) {
+      str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+  }
+
+  str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+      .replace(/\s+/g, '-') // collapse whitespace and replace by -
+      .replace(/-+/g, '-'); // collapse dashes
+
+  return str;
+}
 
 
 
@@ -14,18 +32,39 @@ export default function (ctx, inject, store) {
           const items = [];
       
           const promises = []
+
+          const rootProject = project;
       
           const previewRoot = ctx.$config.previewURL;
+          console.log('project: ', project);
       
           project.subProjects.forEach(project => {
             jszip.folder(project.name);
             project.items.forEach(item => {
               let fileName = item.originalName;
+              
               let fileUrl = `${previewRoot}${item.stems[0].key}.mp3`;
               const prom = this.downloadUrlAsPromise(fileUrl)
               //items.push(item)
+              let nameInZip
+             
+              console.log('fileName: ', fileName);
+
+              console.log('rootProject.settings.publicNames: ', rootProject.settings.publicNames);
+              if (rootProject.settings.publicNames === 1 || rootProject.settings.publicNames === 4) {
+                
+                let newFileName = fileName.replace(/\.[^/.]+$/, "");
+                console.log('newFileName: ', newFileName);
+
+                nameInZip = `${project.name}/${string_to_slug(newFileName)}.mp3`;
+              } else {
+                nameInZip = `${project.name}/${string_to_slug(fileName)}.mp3`;
+                
+              }
+              console.log('nameInZip: ', nameInZip);
+
               jszip.file(
-                `${project.name}/${fileName}.mp3`, //in my case, this produces something like 'my image.jpg'
+                nameInZip, //in my case, this produces something like 'my image.jpg'
                 prom
                 //this.downloadUrlAsPromise(fileUrl) //generates the file we need to download
       
